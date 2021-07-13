d70052bb266285f2e12ee52804dafe0917016e67
Yaowen Xu <x@Macintosh.local>
2021-07-12 22:02:05 -0700

    more htmls

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.new_interaction, name='new_interaction')
+    path('', views.index, name='index'),
+    path('demo/', views.demo, name='demo'),
+    path('new_interaction/', views.new_interaction, name='new_interaction'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('settings/', views.settings, name='settings')
 ]
