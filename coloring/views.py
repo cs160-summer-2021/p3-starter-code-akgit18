9f64d081e48f8af81ac930aa50cf919ca2daae06
Aleksander Kozminski <alek.k@berkeley.edu>
2021-07-14 00:15:26 -0700

    update views

--- coloring/views.py
+++ coloring/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'coloring/index.html')
+    return render(request, 'coloring/homepage.html')
 
 def demo(request):
-    return render(request, 'coloring/demo.html')
+    return render(request, 'coloring/homepage.html')
 
 def new_interaction(request):
     return render(request, 'coloring/new_interaction.html')
